da536ccbcfbef48a2378e8fcbcf81d1db8a8d6d3
Ina <inayumi7@gmail.com>
2024-01-30 16:28:58 +1300

    Initial commit

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator;
+
+/**
+ *
+ * @author Yuna
+ */
+
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
