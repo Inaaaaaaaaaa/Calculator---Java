e05d3fb1b53a549c314f2dab1bd7abcda76d1f1c
Ina <inayumi7@gmail.com>
2024-01-30 16:34:27 +1300

    Creating Calculator.java
    - Creating a new frame where variables are created to construct calculator
    
    - Fonts of buttons are added which can be changed at any time

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -4,16 +4,37 @@
  */
 package calculator;
 
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.*;
+
 /**
  *
  * @author Yuna
  */
 
 
-public class Calculator {
-
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class Calculator implements ActionListener{
     
+    JFrame frame;
+    JTextField textField;
+    JButton[] numberBtn = new JButton[10];
+    JButton[] functionBtn = new JButton[10];
+    JButton addBtn, subBtn, multiBtn, divBtn;
+    JButton decBtn, equalBtn, delBtn, clearBtn;
+    JPanel panel;
+    
+    //font of buttons
+    Font myFont = new Font("Ink Free", Font.BOLD, 30);
+    char operator;
+    
+
+    Calculator()
+    {
+        
+    }
+    public static void main(String[] args) {
+        
+        Calculator calc = new Calculator();
+    }
 }
